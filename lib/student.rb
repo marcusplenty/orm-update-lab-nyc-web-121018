ae4baeb3495be9652e1d222ed27ae3693c0d9609
Marcus Plenty <marcusplenty@gmail.com>
2018-12-29 23:53:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,14 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_reader :id
+  attr_accessor :name, :grade
+  def initialize(name,grade, id = nil)
+    @name = name
+    @grade = grade
+  end 
+  
+  def
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
