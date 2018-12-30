a25e5f1c56c98dcf32f5eed8883809170c72ab9c
Marcus Plenty <marcusplenty@gmail.com>
2018-12-30 00:00:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,9 +24,15 @@ class Student
     SQL
     DB[:conn].execute(sql)
   end 
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  
+  def save
+    sql = <<-SQL
+      INSERT INTO student(name,grade)
+      VALUES(?,?)
+    SQL
+    DB[:conn].execute(sql,self.name,self.grade)
+    @id = 
+  end
 
 
 end
